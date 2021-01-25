cdc36b5b4bcc1a8d11b25a5c865a8eed3cd81033
Ryan Chaffee <rchaff7@yahoo.com>
2021-01-25 12:31:44 -0600

    Add project files.

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace liftoff_storefront.Controllers
+{
+    public class HelloController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("HELLO WORLD");
+        }
+
+        public IActionResult Goodbye()
+        {
+            return Content("GOODBYE WORLD");
+        }
+    }
+}
