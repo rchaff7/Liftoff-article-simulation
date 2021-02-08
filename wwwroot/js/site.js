45044951f5e9081a00cac350893b09f50f7af447
Ryan Chaffee <rchaff7@yahoo.com>
2021-02-07 20:27:23 -0600

    CDN version of darkreader working.

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,10 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+// https://github.com/darkreader/darkreader
+
+DarkReader.enable({
+    brightness: 100,
+    contrast: 90,
+    sepia: 10
+});
